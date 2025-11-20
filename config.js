37472f0ea6ba2bc76ba46b9bae4b718977a537bd
Lyudommuny13 <lyudommuny.kh.13@gmail.com>
2025-11-20 09:28:34 +0700

    update

---
+++ config.js
@@ -0,0 +1,14 @@
+// ==================================================
+// ⚙️ APP CONFIGURATION
+// ==================================================
+
+export const CONFIG = {
+    // PASTE YOUR NEW GOOGLE SCRIPT URL HERE
+    API_URL: 'https://script.google.com/macros/s/AKfycbzZfNb4RfIKrGYM-LazpmtszBcfGGmiHGNV7Z422W88dF0y77UkfwEexW14k7LqpPVj/exec',
+    
+    // Refresh rate for chat (in milliseconds)
+    POLLING_RATE: 2000,
+    
+    // App Name
+    APP_NAME: 'Messenger Clone'
+};
